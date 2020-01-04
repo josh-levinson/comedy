e3bf5980b9dbede287d0ab95a6ce3d25e137a735
Josh Levinson <joshua.levinson@gmail.com>
2020-01-04 11:55:46 -0500

    Small updates to config, added image

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,8 +56,8 @@ module.exports = {
         auth: false,
         name: "items",
         entityLevel: "items",
-        },
       },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
