5d32292af7ad8390834c1c9492ec0f6a2f7eda67
Josh Levinson <joshua.levinson@gmail.com>
2020-01-18 11:19:48 -0500

    Added default image flyer, added non-default image as wells

--- src/pages/item.js
+++ src/pages/item.js
@@ -1,17 +1,36 @@
-import React from "react";
-import { graphql, Link } from "gatsby";
+import React from "react"
+import { graphql, Link } from "gatsby"
+import Img from "gatsby-image"
 import Layout from "../components/layout"
 
-const Item = props => (
+const Item = props => {
+  console.log(props);
+  return (
   <Layout>
     <div>
       <h1>{props.pageContext.summary}</h1>
+      <Img fixed={props.data.file.childImageSharp.fixed} />
       <h3>{props.pageContext.location}</h3>
       <h3>{props.pageContext.start}</h3>
       <p>{props.pageContext.description}</p>
       <Link to="/">Home</Link>
     </div>
   </Layout>
-);
+)
+}
 
 export default Item;
+
+export const query = graphql`
+  query {
+    file(relativePath: { eq: "default_flyer.jpg" }) {
+      childImageSharp {
+        # Specify the image processing specifications right in the query.
+        # Makes it trivial to update as your page's design changes.
+        fixed(width: 750, height: 750) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+  }
+`
