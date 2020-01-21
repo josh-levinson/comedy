52d0686ad6e769cd2b6806838a574a871249f551
Josh Levinson <joshua.levinson@gmail.com>
2020-01-21 16:27:40 -0500

    Put date above location for blurbs

--- src/components/show_blurb.js
+++ src/components/show_blurb.js
@@ -5,9 +5,15 @@ import style from "./show_blurb.module.css"
 export default props => (
   <div className={style.container}>
     <div className={style.showBlurb}>
-      <Link to={props.alternative_id}>{props.summary}</Link><br />
-      {props.location}<br />
-      {new Date(props.dateTime).toLocaleString()}
+      <div>
+        <Link to={props.alternative_id}>{props.summary}</Link>
+      </div>
+      <div>
+        {new Date(props.dateTime).toLocaleString()}
+      </div>
+      <div>
+        {props.location}
+      </div>
     </div>
   </div>
 )
