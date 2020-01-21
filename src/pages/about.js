0bfec3fe05748eb3164e3e8f8fd64c8936eb62e2
Josh Levinson <joshua.levinson@gmail.com>
2020-01-21 13:40:46 -0500

    Fixed typos on about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,9 +9,9 @@ const About = () => (
     <SEO title="About" />
     <h1>About Josh Levinson.</h1>
     <p>Josh Levinson is an up and coming stand up comic who has performed in clubs and bars all over New England. 
-    Software engineer by day and and comedian by night, audiences enjoy Josh's unassuming demeanor as he turns 
+    Software engineer by day and comedian by night, audiences enjoy Josh's unassuming demeanor as he turns 
     his insecurities into punchlines. Whether he's lending his sense of humor to the popular blog 
-    <a href="https://betweentworocks.com" target="_blank">Between Two Rocks</a>
+    <a href="https://betweentworocks.com" target="_blank"> Between Two Rocks </a>
     or performing on stage, he gives you more than the typical IT guy next door. Josh is able to laugh at himself 
     in a way that is infectious and gets you to laugh along with him.</p>
     <Link to="/">home</Link>
