fe5560cfbee8e56b1bc5767b605fc6568dc34f3a
Josh Levinson <joshua.levinson@gmail.com>
2020-02-21 18:05:52 -0500

    Added headshot to homepage sort of

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,24 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import ShowBlurb from "../components/show_blurb"
+import Img from "gatsby-image"
+import { Pane, Heading } from "evergreen-ui"
 
 const IndexPage = ({data}) => {
   return (
     <Layout>
       <SEO title="Home" />
-      <h1>Welcome, humans.</h1>
+      <Pane>
+        <Pane
+          alignItems="center"   
+        >
+          <Heading size={500}>Josh</Heading>
+          <Heading size={500}>Levinson</Heading>
+        </Pane>
+        <Pane>
+       		<Img fixed={data.file.childImageSharp.fixed} />   
+        </Pane>
+      </Pane>
       <h1>Upcoming Shows</h1>
       {data.allItems.edges.map(({ node }, index) => (
         <ShowBlurb 
@@ -41,5 +53,12 @@ export const query = graphql`
         }
       }
     }
+   	file(relativePath: { eq: "josh-pink.jpg" }) {
+      childImageSharp {
+        fixed(width: 750, height: 750) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    } 
   }
 `
