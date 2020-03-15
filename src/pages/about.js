b2155c23aa44829f157f060b9c56079ea6cf4ad3
Josh Levinson <jlevnhv@outlook.com>
2020-03-15 12:24:42 -0400

    idk

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { Pane, Heading, Paragraph, Link } from "evergreen-ui"
@@ -11,19 +10,20 @@ const About = () => (
         display="flex"
         flexDirection="column"
         alignItems="center"
+        justifyContent="center"
         marginTop={25}
+        width="75%"
     >
       <Heading size={800}>About Josh Levinson</Heading>
       <Paragraph marginTop="default">
         Josh Levinson is an up and coming stand up comic who has performed in clubs and bars all over New England. 
         Software engineer by day and comedian by night, audiences enjoy Josh's unassuming demeanor as he turns 
         his insecurities into punchlines. Whether he's lending his sense of humor to the popular blog 
-        <Link href="https://betweentworocks.com" target="_blank">Between Two Rocks</Link>
+        <Link href="https://betweentworocks.com" target="_blank" rel="noopener noreferrer">Between Two Rocks</Link>
         or performing on stage, he gives you more than the typical IT guy next door. Josh is able to laugh at himself 
         in a way that is infectious and gets you to laugh along with him.
       </Paragraph>
     </Pane>
-    <Link to="/">home</Link>
   </Layout>
 )
 
