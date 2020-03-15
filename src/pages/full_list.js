b701c59b6d0743fa79db4beabb597d989045e484
Josh Levinson <jlevnhv@outlook.com>
2020-03-15 12:17:04 -0400

    Additional formatting and links etc

--- src/pages/full_list.js
+++ src/pages/full_list.js
@@ -1,22 +1,30 @@
 import React from "react"
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import ShowBlurb from "../components/show_blurb"
+import { Pane, Text } from "evergreen-ui"
 
 const FullListPage = ({data}) => {
   return (
     <Layout>
       <SEO title="Full Show List" />
       <h1>All Upcoming Shows</h1>
-      {data.allItems.edges.map(({ node }, index) => (
-        <ShowBlurb 
-          alternative_id={node.alternative_id}
-          summary={node.summary}
-          location={node.location}
-          dateTime={node.start.dateTime}
-        />
-      ))}
+      <Pane
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        marginTop={25}
+      >
+       
+        {data.allItems.edges.map(({ node }, index) => (
+          <ShowBlurb 
+            alternative_id={node.alternative_id}
+            summary={node.summary}
+            location={node.location}
+            dateTime={node.start.dateTime}
+          />
+        ))}
+      </Pane>
     </Layout>
   )
 }
